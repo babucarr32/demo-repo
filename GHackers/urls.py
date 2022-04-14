92e1c9ef900ae8e7696683ecd0cd2fcdc88235c3
babucarr32 <baboucarry@gmail.com>
2022-04-14 21:46:13 +0100

    Added a static folder cause I had some issues deploying my app

--- GHackers/urls.py
+++ GHackers/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 import accounts.views
 import Challenges.views
@@ -10,4 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('challenges/', include('Challenges.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
